0f31cb6ffba061d78a5485d36b1267687521276c
KristinaValushka <krystsinavalushka@yahoo.com>
2020-06-21 18:10:34 -0700

    4.1. Аргументы [#309843]

---
+++ src/ru/job4j/calculator/Method.java
@@ -0,0 +1,20 @@
+package ru.job4j.calculator;
+
+public class Method {
+    public static void hello(String name) {
+        System.out.println("Hello, " + name);
+    }
+
+    public static void main(String[] args) {
+        String name = "Kristina Valushko";
+        int age = 33;
+
+        Method.hello(name);
+
+        Method.hello(name);
+
+        Method.hello(name);
+
+        Method.hello(name);
+    }
+}
