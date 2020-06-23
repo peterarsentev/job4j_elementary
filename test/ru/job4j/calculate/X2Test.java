53fc9a79a06d433c070df559c9919c205028be4b
KristinaValushka <krystsinavalushka@yahoo.com>
2020-06-23 14:54:30 -0700

    1.1. Входные данные в тесте [#309875]

---
+++ test/ru/job4j/calculate/X2Test.java
@@ -0,0 +1,55 @@
+package ru.job4j.calculate;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class X2Test {
+
+    @Test
+    public void whenAllNumberOne() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 40;
+        int rsl = X2.calc(a, b, c, x);
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenBCXOne() {
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 40;
+        int rsl = X2.calc(a, b, c, x);
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenABXOne() {
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        int x = 1;
+        int expected = 40;
+        int rsl = X2.calc(a, b, c, x);
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenABCOne() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 0;
+        int expected = 40;
+        int rsl = X2.calc(a, b, c, x);
+        assertThat(rsl, is(expected));
+    }
+}
