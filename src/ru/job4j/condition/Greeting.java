6038ea43449d9e18cb0fd382f5e74bfa1b1ceee1
KristinaValushka <krystsinavalushka@yahoo.com>
2020-06-23 12:27:25 -0700

    9.2. Checkstyle [#309847]

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -2,9 +2,11 @@ package ru.job4j.condition;
 
 public class Greeting {
     public static void main(String[] args) {
-        String idea = "I like Java!";
+        String idea = "I like Java!" + " ";
+        System.out.println(idea);
+        idea = idea + "But I am a newbie.";
         int year = 2020;
-        System.out.println(idea + " " + "But I am a newbie." + " " + year);
+        System.out.println(idea + " " + year);
 
     }
 }
