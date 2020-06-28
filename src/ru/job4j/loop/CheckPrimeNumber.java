80f4f6e459084303db9019bd330c697ee215645b
KristinaValushka <krystsinavalushka@yahoo.com>
2020-06-27 17:40:17 -0700

    Оператор прерывания цикла - break

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -4,7 +4,9 @@ public class CheckPrimeNumber {
 
     public static boolean check(int number) {
         boolean f = true;
-        if (number <= 2) return false;
+        if (number <= 2) {
+            return false;
+        }
         for (int index = 2; index < number; index++) {
             if ((number % index) == 0) {
                 f = false;
