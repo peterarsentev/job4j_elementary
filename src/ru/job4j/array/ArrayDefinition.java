8418e6a17ce185688f73e27bd836ec614875a7a3
KristinaValushka <krystsinavalushka@yahoo.com>
2020-07-01 20:26:55 -0700

    6.0.2. Заполнение массива. [#309817]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -2,8 +2,16 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        String[] surname = new String[100500];
-        float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Kristina Valushka";
+        names[2] = "Alex Pogotski";
+        names[3] = "Tania Pushka";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
     }
-}
+        }
