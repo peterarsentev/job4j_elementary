eefa0e00868c853ef3adba7a4d1c6cc9fb54fdcf
Fotonic1 <VolkovWladislav@mail.ru>
2020-07-02 20:39:44 +0400

    3.3. Разбор первой программы. [#317994]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Vladislav Volkov");
+        System.out.println("07.04.1992");
+    }
+}
