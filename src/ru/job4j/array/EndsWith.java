04e13ca9ffbe13e092760c4a9478ee2792d1c3c3
KristinaValushka <krystsinavalushka@yahoo.com>
2020-07-09 18:01:24 -0700

    6.5.2. Поиск минимума в диапазоне. [#309813]

--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -6,6 +6,7 @@ public class EndsWith {
             for (int i = 0; i < post.length; i++) {
                 if (word[word.length - i - 1] != post[post.length - i - 1]) {
                     result = false;
+                    break;
                 }
             }
 
