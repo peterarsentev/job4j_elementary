7cbea80583d8612908ea1f549ee78d4ce4b0842d
Amelgal <crownedjokerrur@gmail.com>
2020-11-19 22:50:34 +0700

    Модульные тесты. [#392892]

---
+++ test/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.converter;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConverterTest {
+
+    @Test
+    public void whenConvert140RblThen2Euro() {
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubleToEuro(in);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenConvert180RblThen3Dlr() {
+        int in = 180;
+        float expected = 3;
+        float out = Converter.rubleToDollar(in);
+        Assert.assertEquals(expected, out);
+    }
+}
