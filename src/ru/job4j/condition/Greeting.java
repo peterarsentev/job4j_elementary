52c24f7eabf19372c930106757b8e54e5f6e2f8e
Aleks <no>
2020-12-06 18:35:28 +0300

    9.1. String

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java! ";
+        int year = 2020;
+        idea = idea  + "But I am a newbie. " + year;
+        System.out.println(idea);
+
+
+
+    }
+}
