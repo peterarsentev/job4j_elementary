95002f1deb90de7d0552dbd4ebd3a491e0031fef
PoymaLana <marikakopina@gmail.com>
2020-12-23 21:03:00 +0300

    Converter!

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Lana Sergeevna");
+        System.out.println("27.07.1994");
+    }
+}
