a40aee61ad59df48c8e758b56ec6a0d2ed5da9df
PoymaLana <marikakopina@gmail.com>
2020-12-23 21:54:08 +0300

    Fit

---
+++ src/ru/job4j/calculator/Fit.java
@@ -0,0 +1,32 @@
+package ru.job4j.calculator;
+
+public class Fit {
+    public static double manWeight(short height) {
+        int coef=100;
+        double rsl = (height-coef)*1.15;
+        return rsl;
+        /*or
+        double rsl = (height-100)*1.15;
+        return rsl;
+         */
+    }
+
+    public static double womanWeight(short height) {
+        int coef=110;
+        double rsl = (height-coef)*1.15;
+        return rsl;
+        /*or
+        double rsl = (height-110)*1.15;
+        return rsl;
+         */
+    }
+
+    public static void main(String[] args) {
+        short heightMan = 187;
+        short heightWoman = 160;
+        double man = Fit.manWeight(heightMan);
+        double woman = Fit.womanWeight(heightWoman);
+        System.out.println("Man 187 is " + man);
+        System.out.println("Woman 160 is " + woman);
+    }
+}
