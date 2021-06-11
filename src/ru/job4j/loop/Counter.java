98fa330ef425d58ee8e6b768060d4fcc1f7602e8
Amelgal <crownedjokerrur@gmail.com>
2021-06-11 22:19:52 +0700

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -8,11 +8,25 @@ public class Counter {
         }
         return sum;
     }
-
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (int i = start; i <= finish; i++) {
+            if (i % 2 == 0) {
+                sum = sum + i;
+            }
+        }
+        return sum;
+    }
     public static void main(String[] args) {
         System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
+
+        System.out.println("Результат вычисления остатка от деления:" + 21 % 16);
+        System.out.println("Результат вычисления остатка от деления:" + 9 % 12);
+        System.out.println("Результат вычисления остатка от деления:" + 7 % 3);
+        System.out.println("Результат вычисления остатка от деления:" + 15 % 5);
+        System.out.println("Результат вычисления остатка от деления:" + 17 % 3);
     }
 }
 
