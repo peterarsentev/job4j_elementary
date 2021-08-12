8627dc535ca43948ba6c72f02e0b77daefa7b19d
voldomarich <vbekasov100@gmail.com>
2021-08-12 18:32:30 +0300

    9.1. String [#237407]

---
+++ src/ru/job4j/Greeting.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class Greeting {
+        public static void main(String[] args) {
+            String idea = "I like Java!";
+            System.out.println(idea);
+            String id = idea + " " + "But I am a newbie.";
+            int year = 2021;
+            String id2 = idea + " " + year;
+            System.out.println(id);
+            System.out.println(id2);
+        }
+}
