93fd0404cfbc403d48d4250e67328a86fbd8917b
voldomarich <vbekasov100@gmail.com>
2021-08-15 16:06:59 +0300

    4.1.2. Глупый бот [#31323]

---
+++ src/ru/job4j/conditional/DummyBot.java
@@ -0,0 +1,29 @@
+package ru.job4j.conditional;
+
+public class DummyBot {
+    public static String answer(String question) {
+    String rsl = "Это ставит меня в тупик. Задайте другой вопрос";
+        if ("Привет, Бот".equals(question)) {
+            rsl = "Привет, умник";
+        }
+        if ("Пока".equals(question)) {
+            rsl = "До скорой встречи";
+        } else if ("Какой сегодня день недели?".equals(question)) {
+            rsl = "Это ставит меня в тупик. Задайте другой вопрос";
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String rsl = DummyBot.answer("Привет, Бот");
+        System.out.println(rsl);
+        rsl = DummyBot.answer("Пока");
+        System.out.println(rsl);
+        rsl = DummyBot.answer("Какой сегодня день недели?");
+        System.out.println(rsl);
+        rsl = DummyBot.answer("Когда он вернётся?");
+        System.out.println(rsl);
+        rsl = DummyBot.answer("Пока!");
+        System.out.println(rsl);
+    }
+}
