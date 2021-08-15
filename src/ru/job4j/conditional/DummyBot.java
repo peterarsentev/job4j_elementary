54ef0ed5d877a6de2d5d96598982792745d57a7f
voldomarich <vbekasov100@gmail.com>
2021-08-15 21:35:57 +0300

    4.1.2. Глупый бот [#31323]

--- src/ru/job4j/conditional/DummyBot.java
+++ src/ru/job4j/conditional/DummyBot.java
@@ -8,6 +8,9 @@ public class DummyBot {
         }
         if ("Пока".equals(question)) {
             rsl = "До скорой встречи";
+        }
+        if ("Когда он вернётся?".equals(question)) {
+            rsl = "В сентябре либо в начале октября";
         } else if ("Какой сегодня день недели?".equals(question)) {
             rsl = "Это ставит меня в тупик. Задайте другой вопрос";
         }
