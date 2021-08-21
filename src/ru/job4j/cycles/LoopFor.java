b5d625fff29f859d455e350083c5de091120652c
voldomarich <vbekasov100@gmail.com>
2021-08-21 17:19:16 +0300

    5.0. Цикл for. Определение. [#3572]

---
+++ src/ru/job4j/cycles/LoopFor.java
@@ -0,0 +1,10 @@
+package ru.job4j.cycles;
+
+public class LoopFor {
+    public static void main(String[] args) {
+        int in;
+        for (in = 5; in <= 10; in++) {
+            System.out.print(in + " ");
+        }
+        }
+    }
