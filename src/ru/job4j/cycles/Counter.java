ff2fce59a455719ffc6ef9d125f5ced2a79408ab
voldomarich <vbekasov100@gmail.com>
2021-08-22 11:03:02 +0300

    5.0.1. Сумма чисел. [#226416 #192681]

--- src/ru/job4j/cycles/Counter.java
+++ src/ru/job4j/cycles/Counter.java
@@ -3,10 +3,10 @@ package ru.job4j.cycles;
 public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
-        for (start = start; start <= finish; start++) {
-            sum = sum + start;
+        for (int i = start; i <= finish; i++) {
+            sum += i;
         }
-        return (sum);
+        return sum;
     }
 
     public static void main(String[] args) {
