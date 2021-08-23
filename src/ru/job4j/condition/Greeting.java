225b97ad56048827371c063a976b0943eca7efa6
a.bonyushko <a.bonyushko@cdek.ru>
2021-08-23 18:45:58 +0300

    9.1. String [#237407]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea = "I like Java!" + "But I am a newbie.";
+        int year = 2021;
+        idea = "I like Java!" + "But I am a newbie." + year;
+        System.out.println(idea);
+
+    }
+}
