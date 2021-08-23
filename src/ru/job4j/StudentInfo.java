35a380ac70e27ec2553f0ec9ecf33a7c7a310f14
a.bonyushko <a.bonyushko@cdek.ru>
2021-08-23 19:05:10 +0300

    9.2. Checkstyle [#4123]

--- src/ru/job4j/StudentInfo.java
+++ src/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         System.out.println("Alex Bonyushko");
         System.out.println("21.06.1988");
     }
