9aa00d2e99a02807cc29fcd5125423af5e9a5f7e
voldomarich <vbekasov100@gmail.com>
2021-08-25 00:30:05 +0300

    5.5. Простое число [#171694]

---
+++ src/ru/job4j/cycles/CheckPrimeNumber.java
@@ -0,0 +1,15 @@
+package ru.job4j.cycles;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean prime = true;
+        for (int i = 2; i < number; i++) {
+            if ((number % i) == 0) {
+                prime = false;
+            }
+            break;
+        }
+            return prime;
+        }
+    }
+
