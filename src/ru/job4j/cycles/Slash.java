e672e306e4cc50652d7c54ec8617686cbd2d278f
voldomarich <vbekasov100@gmail.com>
2021-08-30 01:00:15 +0300

    5.9. Крест в псевдографике. [#177182]

---
+++ src/ru/job4j/cycles/Slash.java
@@ -0,0 +1,28 @@
+package ru.job4j.cycles;
+
+public class Slash {
+    public static void draw(int size) {
+        for (int row = 0; row < size; row++) {
+            for (int cell = 0; cell < size; cell++) {
+                boolean left = row == cell;
+                boolean right = row + cell == size - 1;
+                if (left) {
+                    System.out.print("X");
+                } else if (right) {
+                    System.out.print("X");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Draw by 3");
+        draw(3);
+        System.out.println("Draw by 5");
+        draw(5);
+    }
+}
