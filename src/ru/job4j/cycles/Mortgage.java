1ff5ace7af7594238ad50c04eb6a65edadcf7c31
voldomarich <vbekasov100@gmail.com>
2021-08-31 16:33:38 +0300

    5.7. Ипотека [#156315 #196425]

--- src/ru/job4j/cycles/Mortgage.java
+++ src/ru/job4j/cycles/Mortgage.java
@@ -4,14 +4,9 @@ public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
         while (amount > 0) {
-            amount = amount + amount * percent - salary;
+            amount += amount * percent / 100 - salary;
             year++;
         }
         return year;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(year(100, 120, 0.5));
-        System.out.println(year(300, 120, 0.3));
     }
 }
