56eb81da18fedaad288c6037ba345da115fd849e
voldomarich <vbekasov100@gmail.com>
2021-09-01 11:50:47 +0300

    6.4.2. Слово заканчивается на ... [#156317]

---
+++ src/ru/job4j/array/EndsWith.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = true;
+            for (int i = 1; i < 5; i++) {
+                if (word[word.length - i] != post[post.length - i]) {
+                    result = false;
+                    break;
+                }
+            }
+        return result;
+    }
+}
---
+++ test/ru/job4j/array/EndsWithTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class EndsWithTest {
+
+    @Test
+    public void whenEndWithPrefixThenTrue() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l', 'o'};
+        boolean result = EndsWith.endsWith(word, post);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void whenNotEndWithPrefixThenFalse() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l', 'a'};
+        boolean result = EndsWith.endsWith(word, post);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void whenNotEndWithThenFalse() {
+        char[] word = {'S', 'e', 'p', 't', 'e'};
+        char[] post = {'l', 'p', 't', 'e'};
+        boolean result = EndsWith.endsWith(word, post);
+        Assert.assertFalse(result);
+    }
+}
