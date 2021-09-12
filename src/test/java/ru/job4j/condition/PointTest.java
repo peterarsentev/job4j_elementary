75133eb006006be3650e2be4914d9f7687459057
a.bonyushko <a.bonyushko@cdek.ru>
2021-09-12 21:07:55 +0300

    4.2. Тернарное сравнение [#189]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -1,9 +1,7 @@
 package ru.job4j.condition;
 
-import org.junit.Assert;
-import org.junit.Test;
-
-import static org.junit.Assert.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class PointTest {
 
