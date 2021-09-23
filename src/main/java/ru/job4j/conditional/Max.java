1f597e5a027adbba6131605e97ee5b692e17dd25
voldomarich <vbekasov100@gmail.com>
2021-09-23 19:14:59 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/conditional/Max.java
+++ src/main/java/ru/job4j/conditional/Max.java
@@ -1,17 +1,32 @@
 package ru.job4j.conditional;
 
 public class Max {
-    public static int max(int left, int right) {
-        boolean x = left > right;
-        return x ? left : right;
+    public static int max(int first, int second) {
+        return first > second ? first : second;
+    }
+
+    public static int max(int first, int second, int third) {
+        return max(first, max(second, third));
+    }
+
+    public static int max(int first, int second, int third, int forth) {
+        return max(max(first, second), max(third, forth));
+    }
+
+    public static int max(int first, int second, int third, int forth, int fifth) {
+        return max(fifth, max(first, second), max(third, forth));
+    }
+
+    public static int max(int first, int second, int third, int forth, int fifth, int sixth) {
+        return max(max(first, second), max(third, forth), max(fifth, sixth));
     }
 
     public static void main(String[] args) {
-        int a = Max.max(33, 110);
-        System.out.println(a);
-        int b = Max.max(21, 21);
-        System.out.println(b);
-        int c = Max.max(-1, -11);
-        System.out.println(c);
+        System.out.println(Max.max(33, 110));
+        System.out.println(Max.max(33, 87, 49));
+        System.out.println(Max.max(21, 18, 150, 150));
+        System.out.println(Max.max(-10133, -111, -221, -100));
+        System.out.println(Max.max(21, 18, 33, 11, 24));
+        System.out.println(Max.max(90, 80, 70, 60, 50, 20));
     }
 }
---
+++ src/test/java/job4j/conditional/MaxTest.java
@@ -0,0 +1,41 @@
+package job4j.conditional;
+
+import org.junit.Assert;
+import org.junit.Test;
+import ru.job4j.conditional.Max;
+
+public class MaxTest {
+
+    @Test
+    public void whenmaxequal() {
+        int first = 110;
+        int second = 110;
+        int result = Max.max(first, second);
+        int expected = 110;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenmaxsixth() {
+        int first = 87;
+        int second = 21;
+        int third = 34;
+        int forth = 29;
+        int fifth = 14;
+        int sixth = 89;
+        int result = Max.max(first, second, third, forth, fifth, sixth);
+        int expected = 89;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenmaxthird() {
+        int first = -1;
+        int second = -11;
+        int third = 250;
+        int forth = 108;
+        int result = Max.max(first, second, third, forth);
+        int expected = 250;
+        Assert.assertEquals(result, expected);
+    }
+}
