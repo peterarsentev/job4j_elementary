2b58eae9d5bca5ae37035209f03051ecfb758f33
voldomarich <vbekasov100@gmail.com>
2021-09-29 23:28:59 +0300

    0.4. Пользовательские исключения. [#219367 #208860]

--- src/main/java/ru/job4j/ex/ElementNotFoundException.java
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -1,7 +1,5 @@
 package ru.job4j.ex;
 
-import java.util.Objects;
-
 public class ElementNotFoundException extends Exception {
         public ElementNotFoundException(String message) {
             super(message);
--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,21 +1,22 @@
 package ru.job4j.ex;
 
 public class FindEl {
-    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+    public static void indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
         for (int el = 0; el < value.length; el++) {
             if (value[el].equals(key)) {
                 rsl = el;
-            } else {
-                throw new ElementNotFoundException("Element could not be found");
+                break;
             }
         }
-        return rsl;
+        if (rsl == -1) {
+        throw new ElementNotFoundException("Element could not be found");
+        }
     }
 
     public static void main(String[] args) {
         try {
-            indexOf(new String[]{"Barcelona", "Rio", "Paris"}, "Barcelona");
+            indexOf(new String[]{"Barcelona", "Rio", "Paris"}, "Lisbon");
         } catch (ElementNotFoundException e) {
             e.printStackTrace();
         }
