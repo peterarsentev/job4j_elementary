f4ac0a030ce213b0a7579f27a9da69588ecbdc8a
DESKTOP-PCK1LER\X220 <a.bonyushko@gmail.com>
2021-10-15 16:09:50 +0300

    x220

--- src/main/java/ru/job4j/StudentInfo.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j;
-
-public class StudentInfo {
-    public static void main(String[] args) {
-        System.out.println("Alex Bonyushko");
-        System.out.println("21.06.1988");
-    }
-}
