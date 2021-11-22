5cdf65564399a2f9c3bd2fae6043e5eb15714e6a
a.bonyushko <a.bonyushko@cdek.ru>
2021-11-22 19:27:49 +0300

    4.4. Вложенный оператор if else. Запутанный код [#190 #216435] - исправления

--- src/test/java/ru/job4j/condition/SqAreaTest.java
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -9,7 +9,6 @@ public class SqAreaTest {
     public void whenP6K2Square2() {
         int expected = 2;
         int p = 6;
-        //double k = 2;
         int k = 2;
         double out = SqArea.square(p, k);
         Assert.assertEquals(expected, out, 0.01);
@@ -19,7 +18,6 @@ public class SqAreaTest {
     public void whenP4K1Square1() {
         int expected = 1;
         int p = 4;
-        //double k = 1;
         int k = 1;
         double out = SqArea.square(p, k);
         Assert.assertEquals(expected, out, 0.01);
@@ -29,7 +27,6 @@ public class SqAreaTest {
     public void whenP10K3Square46875() {
         double expected = 4.6875;
         int p = 10;
-        //double k = 3;
         int k = 3;
         double out = SqArea.square(p, k);
         Assert.assertEquals(expected, out, 0.0001);
