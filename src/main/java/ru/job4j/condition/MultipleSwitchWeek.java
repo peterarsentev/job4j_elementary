41f161af1c850384c8a37df3ea93d6bc9661d1ef
a.bonyushko <a.bonyushko@cdek.ru>
2021-11-24 10:02:16 +0300

    4.6.2. Switch - matcher [#334071]

---
+++ src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
@@ -0,0 +1,24 @@
+package ru.job4j.condition;
+
+/**
+ * 4.6.2. Switch - matcher [#334071]
+ */
+
+public class MultipleSwitchWeek {
+    public static int numberOfDay(String name) {
+        return switch (name) {
+            case "Понедельник", "Monday" -> 1;
+            case "Вторник", "Tuesday" -> 2;
+            case "Среда", "Wednesday" -> 3;
+            case "Четверг", "Thuesday" -> 4;
+            case "Пятница", "Friday" -> 5;
+            case "Суббота", "Saturday" -> 6;
+            case "Воскресенье", "Sunday" -> 7;
+            default -> -1;
+        };
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numberOfDay("Friday"));
+    }
+}
