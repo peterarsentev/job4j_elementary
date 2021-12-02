a2fb212fa22f72598f3c500bce29c477d05a294f
a.bonyushko <a.bonyushko@cdek.ru>
2021-12-02 21:15:53 +0300

    5.0. Цикл for. Определение. [#3572]

---
+++ src/main/java/ru/job4j/loop/LoopFor.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+/**
+ * 5.0. Цикл for. Определение. [#3572]
+ */
+
+public class LoopFor {
+    public static void main(String[] args) {
+        int i;
+        boolean result;
+        for (i = 5; result = i <= 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Индекс после цикла равен: " + i);
+        System.out.println("Условие выполнения цикла равно: " + result);
+    }
+}
