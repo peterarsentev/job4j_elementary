773fe00171ea3978a5d1febd772b0a63d7476718
voldomarich <vbekasov1987@gmail.com>
2023-10-28 12:10:33 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,25 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+
+        private final int sizeOfEmpty = 10;
+        String name;
+        public String surname;
+
+        public static final String NEW_VALUE = "";
+
+        void echo() {
+        }
+
+        void media(Object obj) {
+            if (obj != null) {
+                System.out.println(obj);
+            }
+        }
+
+        void method(int a, int b, int c, int f, int k, int j, int n) {
+        }
+
+        Broken() {
+        }
+    }
